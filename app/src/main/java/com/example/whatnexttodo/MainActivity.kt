6c7985dbd9454009f394ab83fb12bfdf28e99516
niww <niww@ya.ru>
2020-03-24 22:12:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/whatnexttodo/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.whatnexttodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var adapter: RecyclerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        rv.layoutManager = linearLayoutManager
+        adapter = RecyclerAdapter()
+        rv.adapter = adapter
+
+    }
+
+
+}

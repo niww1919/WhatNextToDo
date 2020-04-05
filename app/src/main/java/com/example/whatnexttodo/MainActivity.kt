1aff1aa0a4a9b2118022e55ba58fed3c0ec5fa57
niww <niww@ya.ru>
2020-04-05 23:38:58 +0300

    0.1

--- app/src/main/java/com/example/whatnexttodo/MainActivity.kt
+++ app/src/main/java/com/example/whatnexttodo/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.whatnexttodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +21,31 @@ class MainActivity : AppCompatActivity() {
         adapter = RecyclerAdapter()
         rv.adapter = adapter
 
+        val itemSwipe = ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return true
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    if (direction == ItemTouchHelper.LEFT) {
+                        Toast.makeText(this@MainActivity, "Left", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this@MainActivity, "Right", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            })
+
+        itemSwipe.attachToRecyclerView(rv)
+
     }
+
+
 
 
 }
--- app/src/main/java/com/example/whatnexttodo/RecyclerAdapter.kt
+++ app/src/main/java/com/example/whatnexttodo/RecyclerAdapter.kt
@@ -1,33 +1,40 @@
 package com.example.whatnexttodo
 
+import android.app.Activity
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 
-class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.PhotoHolder>() {
+class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private val texts: Array<String> = arrayOf("334","dfdf", "text")
+    private val texts: MutableList<String> = mutableListOf("334","My test", "text", "Work", " HOW", " Toast", "ViewHolder")
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.PhotoHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflatedView = parent.inflate(R.layout.recyclerview_item_row, false)
-        return PhotoHolder(inflatedView)
+        return ViewHolder(inflatedView)
     }
 
     override fun getItemCount(): Int = texts.size
 
 
-    override fun onBindViewHolder(holder: RecyclerAdapter.PhotoHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.text= texts[position]
+
+
 
 
     }
-    class PhotoHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
+    class ViewHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
 
         private var view: View = v
+        var text = v.findViewById<TextView>(R.id.text)
 
         init {
             v.setOnClickListener(this)
